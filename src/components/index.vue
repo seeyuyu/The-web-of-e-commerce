9352bc072cb1c9c0f6b75911dbecbe27a81e8476
yangyuting <seeyuyu@163.com>
2018-11-16 21:41:26 +0800

    vuecli

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,6 +1,44 @@
 <template>
+<div class="navDiv">
+  <div class="navLi">
+    <p>导航导航</p>
+  </div>
+  <div class="navLi">
+    <p>导航导航</p>
+  </div>
+  <div class="navLi">
+    <p>导航导航</p>
+  </div>
+  <div class="navLi">
+    <p>导航导航</p>
+  </div>
+  <div class="navLi">
+    <p>导航导航</p>
+  </div>
+  <div class="navLi">
+    <p>导航导航</p>
+  </div>
+  <div class="navLi">
+    <p>导航导航</p>
+  </div>
+</div>
 </template>
 <style>
+.navDiv{
+  display: -webkit-flex; /* Safari */
+  display: flex;
+  flex-wrap:wrap;
+  flex-direction: row;
+  width: 100%;
+}
+.navLi{
+  height: 100px;
+  width: 25%;
+  background: url(http://static1.xlzhao.com/frontend/images/navgroup1.png) no-repeat center 16px;
+  background-size: 0.8rem 0.8rem;
+  padding-top:70px;
+}
+
 </style>
 <script>
 export default {
