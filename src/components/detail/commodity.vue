367b91dc4b7e3504e4219f0d0e8cb2d6bf3426d8
yangyuting <seeyuyu@163.com>
2018-12-08 23:13:29 +0800

    个人中心

--- src/components/detail/commodity.vue
+++ src/components/detail/commodity.vue
@@ -1,11 +1,39 @@
 <template>
   <div>
-    <div></div>
-
-    <footnav :idx="3"></footnav>
+    <div>
+      <div></div>
+    </div>
+    <div class="txtLeft comintro">
+      <p class="text_ovh">名称啊名称啊名称啊</p>
+      <div class="comintroTxt">这里是简介啊哈哈哈哈哈哈哈哈哈哈哈哈这里是简介啊哈哈哈哈哈哈哈哈哈哈哈哈这里是简介啊哈哈哈哈哈哈哈哈哈哈哈哈这里是简介啊哈哈哈哈哈哈哈哈哈哈哈哈</div>
+      <div><span class="fl">¥9.00</span><span class="fl">¥9.00</span><span class="fl">特价</span><span class="fr">已售10万+件</span></div>
+    </div>
+    <div>
+      <p>闪电送货</p>
+      <p>马上到马上到马上到</p>
+    </div>
   </div>
 </template>
 <style>
+html{
+  background:#f6f6f6;
+}
+.txtLeft{
+  text-align: left;
+}
+.comintro{
+  padding:15px 10px;
+  background: #fff;
+  margin: 15px 0;
+}
+.comintroTxt{
+  padding:15px 0;
+}
 </style>
 <script>
+export default {
+  data() {
+    return {};
+  }
+};
 </script>
