5c8f439c21f1eb4744f350de95a2be67c48959da
yangyuting <seeyuyu@163.com>
2019-01-10 22:49:41 +0800

    购物车

---
+++ src/components/car/shopcar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="searchout"><search></search></div>
+    <commoditylist></commoditylist>
+    <footnav :idx='2'></footnav>
+  </div>
+</template>
+<style>
+html{
+  background: #f6f6f6;
+}
+.searchout{
+  background: #fff;
+  overflow: hidden;
+}
+.searchout .searchDiv {
+    border-radius: 100px;
+    margin: 5px 10px;
+    background: #f6f6f6;
+}
+</style>
+<script>
+import footnav from "components/footnav/footnav"
+import commoditylist from "components/car/commodity"
+import search from "components/search/searchdiv"
+export default {
+  data(){
+    return{
+
+    }
+  },
+  components:{
+    footnav,
+    search,
+    commoditylist
+  },
+}
+</script>
