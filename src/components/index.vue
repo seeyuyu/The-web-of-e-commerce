99a40d9a8f55f19fa397fc632300a1719c04c416
yangyuting <seeyuyu@163.com>
2019-05-13 23:17:23 +0800

    1

--- src/components/index.vue
+++ src/components/index.vue
@@ -8,10 +8,10 @@
       <p>{{item.name}}</p>
     </div>
   </div>
-  <div class="topNews marquee">
+  <div class="topNews">
     <div class="fl topTitle">多点头条</div>
-    <div class="marquee_box">
-      <ul class="marquee_list" :class="{marquee_top:animate}">
+    <div>
+      <ul>
         <li><a href="###" class="fl topCon text_ovh">推荐！快来围观啊优惠好多啊来围观啊优惠好多啊来围观啊优惠好多啊啊啊啊啊啊啊！</a></li>
         <li><a href="###" class="fl topCon text_ovh">推荐！快来围观啊优惠好多啊来围观啊优惠好多啊来围观啊优惠好多啊啊啊啊啊啊啊！</a></li>
         <li><a href="###" class="fl topCon text_ovh">推荐！快来围观啊优惠好多啊来围观啊优惠好多啊来围观啊优惠好多啊啊啊啊啊啊啊！</a></li>
@@ -172,81 +172,6 @@ html {
   border-radius: 4px;
   text-align: left;
 }
-
-
-
-
-.marquee {
-  width: 100%;
-
-  height: 50px;
-
-  align-items: center;
-
-  color: #3a3a3a;
-
-  background-color: white;
-
-  display: flex;
-
-  box-sizing: border-box;
-
-  overflow: hidden;
-}
-
-.topTitle {
-  padding: 0 20px;
-
-  height: 21px;
-
-  font-size: 14px;
-
-  border-right: 1px solid #d8d8d8;
-
-  align-items: center;
-}
-
-.marquee_box {
-  display: block;
-
-  position: relative;
-
-  width: 60%;
-
-  height: 30px;
-
-  overflow: hidden;
-}
-
-.marquee_list {
-  display: block;
-
-  position: absolute;
-
-  top: 0;
-
-  left: 0;
-}
-
-.marquee_top {
-  transition: all 0.5s;
-
-  margin-top: -30px;
-}
-
-.marquee_list li {
-  height: 30px;
-
-  line-height: 30px;
-
-  font-size: 14px;
-
-  padding-left: 20px;
-}
-
-.marquee_list li span {
-  padding: 0 2px;
-}
 </style>
 <script>
 import axios from "axios";
@@ -311,7 +236,6 @@ export default {
     this.navData();
     this.recommendData();
     // 页面显示
-    setInterval(this.showMarquee, 2000);
   },
   methods: {
     navData: function() {
@@ -336,17 +260,6 @@ export default {
         // console.log(res.data);
         that.recommendli=res.data.data;
       })
-    },
-    showMarquee: function() {
-      this.animate = true;
-
-      setTimeout(() => {
-        this.marqueeList.push(this.marqueeList[0]);
-
-        this.marqueeList.shift();
-
-        this.animate = false;
-      }, 500);
     }
   }
 };
