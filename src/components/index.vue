59adc5020bea0e3c224ebd9181794d0208fa568c
yangyuting <seeyuyu@163.com>
2019-06-05 23:47:27 +0800

    引入mint-ui

--- src/components/index.vue
+++ src/components/index.vue
@@ -71,6 +71,7 @@ html {
 .navLi {
   height: 100px;
   width: 25%;
+
   /* background: url(http://static1.xlzhao.com/frontend/images/navgroup1.png) */
   /* no-repeat center 16px; */
   /* background-size: 0.8rem 0.8rem; */
