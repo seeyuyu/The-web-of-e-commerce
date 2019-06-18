6e2f40b7628ba2d4a367fdb40f9c977b9b2b9a45
yangyuting <seeyuyu@163.com>
2019-06-18 23:16:40 +0800

    1

--- src/components/ce.vue
+++ src/components/ce.vue
@@ -20,27 +20,6 @@ return {
 
   },
   methods: {
-    gologin: function() {
-      // console.log(this.input1)
-      let that = this;
-      let md5 = crypto.createHash("md5");
-      md5.update(that.password); //需要加密的密码
-      let password = md5.digest("hex"); //password 加密完的密码
-      axios
-        .post("/api/employee/login", {
-          username: that.username,
-          password: password
-        })
-        .then(function(res) {
-          console.log(res);
-          if(res.data.code==0){
-            sessionStorage.setItem('userId','123');
-            that.$router.go(-1);
-          }else{
-            alert(res.data.msg);
-          }
-        });
-    }
   }
 };
 </script>
