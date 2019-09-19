dfac778306900ebe7a5ac28ea79f3eefd4fb38b7
yangyuting <seeyuyu@163.com>
2019-09-19 22:57:00 +0800

    封装axios

--- src/api/car.js
+++ src/api/car.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-import http from 'common/js/common'
+import { get, post} from 'common/js/http'
+
 
 /*
   【加入购物车】
@@ -7,12 +8,17 @@ import http from 'common/js/common'
   cteated by yyt 2019/09/18
 */
 export const _addCar = (parameter) => {
-  return axios.post("/api/shopping-cart", {
+  // return service.post("/api/shopping-cart", {
+  //   count: parameter.num,
+  //   _id: parameter.id
+  // }).then((res) => {
+  //   return Promise.resolve(res.data)
+  // }).catch((error) => {
+  //   return Promise.reject(error)
+  // })
+
+  return post("/api/shopping-cart", {
     count: parameter.num,
     _id: parameter.id
-  }).then((res) => {
-    return Promise.resolve(res.data)
-  }).catch((error) => {
-    return Promise.reject(error.response.data)
   })
 }
--- src/common/js/http.js
+++ src/common/js/http.js
@@ -1,30 +1,77 @@
-// 1.加token
-// 2.防止重复提交
-// 3.消息提示
-
+import Vue from 'vue'
 import axios from 'axios'
+// import router from '@/router'
 
-//http request 拦截器
-axios.interceptors.request.use(config => {
-    // config.headers = {
-    //   'Content-Type':'application/x-www-form-urlencoded'
-    // }
-    if (promiseArr[config.url]) {
-      promiseArr[config.url]('操作取消')
-      promiseArr[config.url] = cancel
-    } else {
-      promiseArr[config.url] = cancel
-    }
-    return config
-}, error => {
-    return Promise.reject(err)
-})
-axios.interceptors.response.use(response => {
-  // 获取token并把token放在sessionStorage中,根据实际需求选择存储方式即可
-  let list = response.data.list;      // 根据实际数据结构取得返回的token
-  if (list.hasOwnProperty("token")) {
-    sessionStorage.setItem('token', list.token);
-    axios.defaults.headers.token = sessionStorage.getItem('token');
+// 创建axios实例
+const service = axios.create({
+  timeout: 1000 * 30,
+  // 允许跨域带token
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
   }
-  return response;
 })
+
+// request拦截器
+service.interceptors.request.use(config => {
+  // Do something before request is sent
+  //window.localStorage.getItem("accessToken") 获取token的value
+  let token = window.sessionStorage.getItem("token")
+  if (token) {
+      //将token放到请求头发送给服务器,将tokenkey放在请求头中
+      config.headers.token = token;
+      //也可以这种写法
+      // config.headers['accessToken'] = Token;
+       return config;
+  }
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
+
+// response拦截器
+service.interceptors.response.use(response => {
+  console.log(response)
+  if (response.data && response.data.code === 401) { // 401, token失效
+    Vue.cookie.delete('token')
+    // router.push({name: 'login'})
+    router.push({
+      name:'login',//从哪个页面跳转
+      query:{
+        retUrl:window.location.href.split('#')[1] || '',
+        is_new_user_url:1
+      }
+    })
+  }
+  return response
+}, error => {
+  return Promise.reject(error)
+})
+
+// export default service
+// 封装get
+export const get = (url, params) => {
+  params = params || {};
+  return new Promise((resolve, reject) => {
+    service.get(url, {
+          params,
+      }).then((res) => {
+        return Promise.resolve(res.data)
+      }).catch((error) => {
+        return Promise.reject(error)
+      })
+  })
+}
+//封装post
+export const post = (url, params) => {
+  params = params || {};
+  return new Promise((resolve, reject) => {
+    service.post(url,
+          params
+      ).then((res) => {
+        return Promise.resolve(res.data)
+      }).catch((error) => {
+        return Promise.reject(error)
+      })
+  })
+}
